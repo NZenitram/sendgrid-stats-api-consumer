1c81fd8dadf8552493a843f47b7fc444e023e5a6
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-17 20:37:58 -0600

    global data csv built

--- app/helpers/response.rb
+++ app/helpers/response.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 module Response
 
+
   def self.response(start_date, end_date, key)
     conn = Faraday::Connection.new("https://api.sendgrid.com/v3/mailbox_providers/stats?start_date=#{start_date}&end_date=#{end_date}")
     conn.headers['Authorization'] = "Bearer #{key}"
@@ -11,20 +12,23 @@ module Response
   def self.parse_reponse(conn)
     response = conn.get
     providers = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
     create_header(providers)
   end
 
   def self.create_header(providers)
     file = "response_csv"
+    global = "global_csv"
     header = ["date", "provider", "blocks", "bounces", "clicks", "deferred", "delivered", "drops", "opens", "spam_reports", "unique_clicks", "unique_opens"]
     CSV.open("./tmp/#{file}", "wb") do |csv|
       csv << header
     end
-    gather_data(providers, file)
+    CSV.open("./tmp/#{global}", "wb") do |csv|
+      csv << header
+    end
+    gather_data(providers, file, global)
   end
 
-  def self.gather_data(providers, file)
+  def self.gather_data(providers, file, global)
     providers.each do |metric|
       metric[:stats].each do |i|
         data = []
@@ -33,6 +37,7 @@ module Response
         data.push(i[:metrics].values)
         clean_data = data.flatten
         append_csv(clean_data, file)
+        append_global(clean_data, global)
       end
     end
   end
@@ -40,6 +45,13 @@ module Response
   def self.append_csv(clean_data, file)
     CSV.open("./tmp/#{file}", "ab") do |csv|
       csv << clean_data
+    end
+  end
+
+  def self.append_global(data, global)
+    clean_data = data.delete_at(1)
+    CSV.open("./tmp/#{global}", "ab") do |csv|
+      csv << data
     end
   end
 
