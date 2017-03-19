1dc8b10e652a699a62368503c32643eb5558e718
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-19 15:42:34 -0600

    provider graphs populate

--- app/assets/javascripts/chart_select.js
+++ app/assets/javascripts/chart_select.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
-
+var counter = 0
   $('.multiple-providers').on('click', function appendGraphs(){
     var checked = $('.providerCheckBox:checkbox:checked')
     for (var i = 0; i < checked.length; i++) {
       var provider = checked[i].parentElement.innerText
-    $.ajax({
-      url: "/api/v1/providers/" + provider,
-      type: 'GET',
-      success: function(){
-        var checked = $('.providerCheckBox:checkbox:checked')
-        $("#graphs").append('<div id='+ provider +' class="inline-display graph-data" style="min-width: 310px; height: 400px; margin: 0 auto"></div>')
-        $.get(this.url, function(csv) {
-              $('#'+ provider +'').highcharts({
-                  chart: {
-                      type: 'line'
-                  },
-                  data: {
-                      csv: csv["thing"]
-                  },
-                  title: {
-                      text: provider
-                  },
-                  yAxis: {
-                      title: {
-                          text: 'Units'
-                      }
-                  }
-              });
-          });
-        }
-      })
+      $("#graphs").append('<div id='+ provider +' class="inline-display graph-data" style="min-width: 310px; height: 400px; margin: 0 auto"></div>')
+      populateGraphs(provider)
     }
   })
+
+  function populateGraphs(provider){
+      $.ajax({
+        url: "/api/v1/providers/" + provider,
+        type: 'GET',
+        success: function(){
+          var checked = $('.providerCheckBox:checkbox:checked')
+          $.get(this.url, function(csv) {
+                $('#'+ provider +'').highcharts({
+                    chart: {
+                        type: 'line'
+                    },
+                    data: {
+                        csv: csv["thing"]
+                    },
+                    title: {
+                        text: provider
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Units'
+                        }
+                    }
+                });
+            });
+          }
+        })
+      }
 });
