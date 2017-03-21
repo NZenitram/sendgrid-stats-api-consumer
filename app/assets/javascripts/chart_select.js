358974efc835044a8fe37ad8e9b413673426df49
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-21 12:47:43 -0600

    graph divs being removed

--- app/assets/javascripts/chart_select.js
+++ app/assets/javascripts/chart_select.js
@@ -1,45 +1,49 @@
 $(document).ready(function() {
 var counter = 0
-  $('.multiple-providers').on('click', function appendGraphs(){
-    var checked = $('.providerCheckBox:checkbox:checked')
-    for (var i = 0; i < checked.length; i++) {
-      var provider = checked[i].parentElement.innerText
-      $("#graphs").append('<div id='+ provider +' class="inline-display graph-data" style="min-width: 310px; height: 400px; margin: 0 auto"></div>')
-      populateGraphs(provider)
-    }
-  })
-
-  function populateGraphs(provider){
-      $.ajax({
-        url: "/api/v1/providers/" + provider,
-        type: 'GET',
-        success: function(){
-          var checked = $('.providerCheckBox:checkbox:checked')
-          $.get(this.url, function(csv) {
-                $('#'+ provider +'').highcharts({
-                    chart: {
-                        type: 'line'
-                    },
-                    data: {
-                        csv: csv["thing"]
-                    },
-                    title: {
-                        text: provider
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Units'
-                        }
-                    }
-                });
-            });
-          }
-        })
-      }
+  $('.multiple-providers').on('click', appendGraphs)
 
   $('#clear-button').on('click', clearGraphs)
 });
 
-function clearGraphs(){
-  $('#graphs').children().empty()
+
+function appendGraphs(){
+  clearGraphs()
+ var checked = $('.providerCheckBox:checkbox:checked')
+   for (var i = 0; i < checked.length; i++) {
+     var provider = checked[i].parentElement.innerText
+     $("#graphs").append('<div id='+ provider +' class="inline-display graph-data" style="min-width: 310px; height: 400px; margin: 0 auto"></div>')
+     populateGraphs(provider)
+   }
 }
+
+function clearGraphs(){
+  $('#graphs').children().remove()
+}
+
+function populateGraphs(provider){
+    $.ajax({
+      url: "/api/v1/providers/" + provider,
+      type: 'GET',
+      success: function(){
+        var checked = $('.providerCheckBox:checkbox:checked')
+        $.get(this.url, function(csv) {
+              $('#'+ provider +'').highcharts({
+                  chart: {
+                      type: 'line'
+                  },
+                  data: {
+                      csv: csv["thing"]
+                  },
+                  title: {
+                      text: provider
+                  },
+                  yAxis: {
+                      title: {
+                          text: 'Units'
+                      }
+                  }
+              });
+          });
+        }
+      })
+    }
