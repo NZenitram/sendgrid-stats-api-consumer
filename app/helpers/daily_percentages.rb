ef3d1d46de38f7961d1fc4d2b28b6145fbb7eebc
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-28 20:18:25 -0600

    license

--- app/helpers/daily_percentages.rb
+++ app/helpers/daily_percentages.rb
@@ -82,18 +82,3 @@ module DailyPercentages
     end
   end
 end
-
-class FindPercentage
-  def initialize(event, deliveries)
-    @event = event
-    @deliveries = deliveries
-  end
-
-  def percentage
-    (@event / @deliveries).round(5) * 100
-  end
-
-  def self.not_a_number?(event)
-    (event.is_a?(Float) && event.nan? || event == Float::INFINITY)
-  end
-end
