74d0b16b61ac90ea14daf1fab18c2b9fbe7f7a00
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-28 08:20:27 -0600

    cleaned up file tree

--- app/helpers/date_helper.rb
+++
@@ -1,9 +0,0 @@
-module DateHelper
-
-  def self.correct_date(date)
-    date_array = date.split('/')
-    year = date_array.pop
-    new_date = date_array.unshift(year)
-    new_date.join('-')
-  end
-end
