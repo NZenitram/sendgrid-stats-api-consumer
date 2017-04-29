6de825f8554ad8d6bacc9857fb0739682adeded8
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-29 16:16:22 -0600

    log in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'users/sessions'}
-
-  root to: 'landing#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   get '/welcome', to: 'home#welcome'
   get '/global', to: 'home#index'
