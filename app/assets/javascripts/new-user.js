954fbf5b42b8ae560aab1071370564c4f27977ae
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-04 12:02:23 -0600

    topfive

--- app/assets/javascripts/new-user.js
+++ app/assets/javascripts/new-user.js
@@ -1,7 +1,7 @@
-  var $window = $(window)
-  $stickyEl = $('#login-header');
-  elTop = $stickyEl.offset().top;
-
-$(window).scroll(function() {
-  $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-});
+//   var $window = $(window)
+//   $stickyEl = $('#login-header');
+//   elTop = $stickyEl.offset().top;
+//
+// $(window).scroll(function() {
+//   $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+// });
