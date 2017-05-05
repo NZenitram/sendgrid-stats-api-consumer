606c0c508ca484b98be193187cc9098294068c8d
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-05 17:19:41 -0600

    key push

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :user_name => "apikey",
   :password => "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",
   :address => 'smtp.sendgrid.net',
+  :domain => 'simplymailstatistics.com'
   :port => 587,
   :authentication => :plain,
-  :enable_starttls_auto => true,
-  :ssl => true,
-  :tls => true
+  :enable_starttls_auto => true
 }
