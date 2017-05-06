ecdd80fd40a4650492d3836b3ce7a25bcf7f2846
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-06 17:22:17 -0600

    rebase

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+<<<<<<< HEAD
+=======
+
+>>>>>>> 9b53da1... merge conflicts
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USER_NAME'],
   :password => ENV['SENDGRID_API_KEY'],
