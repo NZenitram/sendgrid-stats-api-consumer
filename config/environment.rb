9b53da1049c91dee63d8a4d353fcbd296c3e09a5
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-06 17:10:14 -0600

    merge conflicts

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USER_NAME'],
+  :password => ENV['SENDGRID_API_KEY'],
+  :address => 'smtp.sendgrid.net',
+  :domain => 'simplymailstatistics.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
